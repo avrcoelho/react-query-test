3061bb16a3f9d4ca791cb4744a395454a1665f76
Andre Coelho <andre.coelho@axur.com>
2020-10-26 15:07:57 -0300

    chore: remove variables not usages

--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { QueryCache, ReactQueryCacheProvider } from 'react-query'
 
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import GlobalStyles from './styles/Global'
-
-const queryCache = new QueryCache({
-  defaultConfig: {
-    queries: {
-      enabled: true
-    }}
-})
 
 ReactDOM.render(
   <React.StrictMode>
